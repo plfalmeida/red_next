b9d893a8b0fb3cea13b7e137326500ff0e0204a9
Pedro Almeida <pedro--luiz@outlook.com>
2022-09-12 09:32:39 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites(){
+    return [
+      {
+        source: '/:path*',
+        destination:'/:path*',
+      },
+      // {
+      //   source: '/:path*',
+      //   destination:'http://localhost:3001/:path*',
+      // },
+      {
+        source: '/:path*',
+        destination:'https://cra-redirects.vercel.app/:path*',
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/api/login.js
+++ pages/api/login.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { nookies, setCookie } from 'nookies';
 
 export default function handler(req, res) {
+  const { name } = req.body
+
+  setCookie({ res }, 'sessionId', name, {
+    maxAge: 30 * 24 * 60 * 60,
+    path: '/',
+  })
+
   res.status(200).json({ name: 'John Doe' })
 }
